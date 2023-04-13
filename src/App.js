3fdf32431d53aa595609ac6a49d7ff978231d208
Bonaventure <bonaogeto1@gmail.com>
2023-04-13 09:12:28 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+const countries = [
+  {
+    name: "Algeria",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Angola",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Benin",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Botswana",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Burkina Faso",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Burundi",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Cameroon",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Cape Verde",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Central African Republic",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Chad",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Comoros",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Democratic Republic of the Congo",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Republic of the Congo",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Djibouti",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Egypt",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Equatorial Guinea",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Eritrea",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Eswatini (Swaziland)",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Ethiopia",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+  {
+    name: "Gabon",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  },
+];
 
 function App() {
+  const [country, setCountry] = useState({
+    name: "Algeria",
+    flag: "https://media.istockphoto.com/id/1213195172/video/kenya-waving-flag-national-3d-kenyan-flag-waving-sign-of-kenya-seamless-loop-animation-kenyan.jpg?s=640x640&k=20&c=kBXwbhydaaugUAoZnUH6yH98KsT-f7LTLGqEDemAe0U=",
+  });
+
+  function handleClick() {
+    const randomCountry =
+      countries[Math.floor(Math.random() * countries.length)];
+    setCountry(randomCountry);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>African Countries</h1>
+      {/* <div className="countries"> */}
+      {/* {countries.map((country) => (
+          <div className="country">
+            <img src={country.flag} alt={country.name} />
+            <h2>{country.name}</h2>
+          </div>
+        ))} */}
+      <div className="country">
+        <img src={country.flag} alt={country.name} />
+        <h2>{country.name}</h2>
+        <button onClick={handleClick}>Next Country</button>
+      </div>
+      {/* </div> */}
     </div>
   );
 }
